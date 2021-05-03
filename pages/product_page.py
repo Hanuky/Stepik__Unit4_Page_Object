352c697099f64f058a0ad1639084e417970798a5
Andrey Gorbanev <gorbanevaa@hotmail.com>
2021-05-03 17:13:21 +0300

    add is_not_present & is_disappeared function

--- pages/product_page.py
+++ pages/product_page.py
@@ -14,7 +14,7 @@ class ProductPage(BasePage):
         return self.browser.find_element(*ProductPageLocators.PRICE).text 
     def chek_product_price_in_basket(self):
         return self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text           
-    def add_product_promo_in_basket(self):
+    def add_product_in_basket(self):
         add_basket = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
         add_basket.click()
     def chek_success_message_after_add_product(self):
@@ -22,7 +22,10 @@ class ProductPage(BasePage):
     def should_be_promo_url(self): 
         assert self.PAGE_SLUG in self.browser.current_url, "Product promo page url is wrong" 
     def chek_price_equals_basket_price(self):
-        assert self.chek_product_price() == self.chek_product_price_in_basket(), "Product price or product price in basket wrong"
+        assert self.chek_product_price() == self.chek_product_price_in_basket(), "Product price or product price in basket not equals"
     def chek_product_name_equal_product_basket_name(self): 
-        assert self.chek_product_name() == self.chek_product_basket_name(), "Product name or product name in basket wrong"      
-     
+        assert self.chek_product_name() == self.chek_product_basket_name(), "Product name or product name in basket not equals" 
+    def guest_should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented"
+    def guest_should_disappeared_be_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented"    
--- test_product_page.py
+++ test_product_page.py
@@ -5,13 +5,32 @@ from .pages.login_page import LoginPage
 from .pages.base_page import BasePage
 from .pages.product_page import ProductPage
 
+'''
 @pytest.mark.parametrize('number_link', ["offer0", "offer1", "offer2", "offer3", "offer4", "offer5", "offer6", pytest.param("offer7", marks=pytest.mark.xfail), "offer8", "offer9"])
 def test_guest_can_add_product_to_basket(browser, number_link):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo={number_link}"
     page = ProductPage(browser, link)
     page.open()
-    page.add_product_promo_in_basket()
+    page.add_product_in_basket()
     page.solve_quiz_and_get_code()
     page.chek_success_message_after_add_product()
     page.chek_price_equals_basket_price()
     page.chek_product_name_equal_product_basket_name()
+'''    
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"   
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_in_basket()
+    page.guest_should_not_be_success_message()
+def test_guest_cant_see_success_message(browser):     
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"   
+    page = ProductPage(browser, link)
+    page.open()
+    page.guest_should_not_be_success_message()
+def test_message_disappeared_after_adding_product_to_basket(browser):     
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"   
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_in_basket()
+    page.guest_should_disappeared_be_success_message()
